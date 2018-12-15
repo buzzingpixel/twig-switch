1ee5987f43f92af844ae6368191e623ca523be65
TJ Draper <tj@buzzingpixel.com>
2018-12-15 16:36:19 -0600

    Add copyright and license info to file banners

--- src/SwitchTwigExtension.php
+++ src/SwitchTwigExtension.php
@@ -1,6 +1,12 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * @author TJ Draper <tj@buzzingpixel.com>
+ * @copyright 2018 BuzzingPixel, LLC
+ * @license Apache-2.0
+ */
+
 namespace buzzingpixel\twigswitch;
 
 use Twig_Extension;
